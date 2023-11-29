28163eda1299bc87d08fa6717cbea39e17b98fbc
Mogawish <119753320+Mogawish@users.noreply.github.com>
2023-11-29 05:25:37 +0300

    Alter table

---
+++ dbo.Contract.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Contract] (
+    [id]      INT           IDENTITY (1, 1) NOT NULL,
+    [surname] NVARCHAR (50) NULL,
+    [name]    NVARCHAR (50) NULL,
+    [payment] MONEY         NULL,
+    [term]    INT           NULL,
+    CONSTRAINT [PK_Contract] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
---
+++ dbo.Status.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Status] (
+    [id]  INT           IDENTITY (1, 1) NOT NULL,
+    [tag] NVARCHAR (50) NULL,
+    CONSTRAINT [PK_Status] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
